167068fd4172f4882dcadd741343eae8edb27c80
Fryansb <ryan.sbv2@gmail.com>
2025-09-20 17:44:42 -0300

    feat(tarefas): permitir edição da descrição e salvar no estado global
    adiciona ação 'editar' no slice 'tarefas'\natualiza 'Tarefa' para modo de edição com controle local e dispatch\najusta UI: input de descrição editável, salvar e cancelar (#1)

--- src/components/Tarefas/index.tsx
+++ src/components/Tarefas/index.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import * as S from './styles'
-import { remover } from '../../store/reducers/tarefas'
+import { remover, editar } from '../../store/reducers/tarefas'
 import TarefaClass from '../../models/Tarefa'
 
 type Props = TarefaClass
 
-const Tarefa = ({ title, priority, description, status, id }: Props) => {
+const Tarefa = ({
+  title,
+  priority,
+  description: descriptionOriginal,
+  status,
+  id
+}: Props) => {
   const dispatch = useDispatch()
   const [estaEditando, setEstaEditando] = useState(false)
+  const [description, setDescription] = useState('')
+
+  useEffect(() => {
+    if (descriptionOriginal.length > 0) {
+      setDescription(descriptionOriginal)
+    }
+  }, [descriptionOriginal])
+
+  function cancelDescriptionEdit() {
+    setEstaEditando(false)
+    setDescription(descriptionOriginal)
+  }
+
   return (
     <S.Card>
       <S.Title>{title}</S.Title>
@@ -19,12 +38,35 @@ const Tarefa = ({ title, priority, description, status, id }: Props) => {
       <S.Tag parameter="status" status={status}>
         {status}
       </S.Tag>
-      <S.Description value={description} />
+      <S.Description
+        disabled={!estaEditando}
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
       <S.ActionBar>
         {estaEditando ? (
           <>
-            <S.ButtonSave>Salvar</S.ButtonSave>
-            <S.ButtonCancelRemove onClick={() => setEstaEditando(false)}>
+            <S.ButtonSave
+              onClick={() => {
+                dispatch(
+                  editar({
+                    description,
+                    id,
+                    priority,
+                    status,
+                    title
+                  })
+                )
+                setEstaEditando(false)
+              }}
+            >
+              Salvar
+            </S.ButtonSave>
+            <S.ButtonCancelRemove
+              onClick={() => {
+                cancelDescriptionEdit()
+              }}
+            >
               Cancelar
             </S.ButtonCancelRemove>
           </>
